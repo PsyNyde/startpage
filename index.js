f32e0652ea6ffa0e767df6195c41c5449261afa9
wahidislamlinad <wahidislamlinad@gmail.com>
2021-12-02 20:34:49 +0600

    ksuna

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+//* Time function
 function showTime() {
 	const date = new Date();
-
 	let today = date.toLocaleString("en", { weekday: "long" });
-	let hour = date.toLocaleString("pl", { hour: "numeric"}); // use 24h time format
+	let hour = date.toLocaleString("pl", { hour: "numeric" }); // uses 24h time format
 	let minute = date.toLocaleString("en", { minute: "2-digit" });
 	let second = date.toLocaleString("en", { second: "2-digit" });
 	let day = date.toLocaleString("en", { day: "2-digit" });
@@ -16,60 +16,114 @@ function showTime() {
 		"date"
 	).innerHTML = `${today}, ${hour}:${minute}:${second} | ${day}/${month}/${year}`;
 	setTimeout(showTime, 0);
-	
 }
-
 function addZero(i) {
 	if (i.length < 2) i = "0" + i;
 	return i;
 }
-
-
-function lol(){
-	const foo = new Date();
-	let hour = foo.toLocaleString("pl", { hour: "numeric"});
-	var nun = parseInt(hour);
-	if (nun >= 6 && nun <= 18){
-		document.getElementById("icon").innerHTML = `filter_drama`;
-	}
-	else if (nun >= 19 && nun<=24){
-		document.getElementById("icon").innerHTML = `nights_stay`;
-	}
-	else if (nun >= 1 && nun <= 5 ){
-		document.getElementById("icon").innerHTML = `nights_stay`;
-	}
-}
-
-
 showTime();
-lol()
 
-const f = document.getElementById('form');
-const q = document.getElementById('query');
-const se = 'https://google.com/search?q=';
-
-function submitted(event) {
-  event.preventDefault();
-  const url = se + q.value;
-  const win = window.open(url, '_blank');
-  q.value = "";
-  win.focus();
-}
-
-f.addEventListener('submit', submitted);
-
-var myPix = new Array("assets/1.jpg","assets/2.jpg","assets/3.jpg","assets/4.jpg","assets/5.jpg");
+//* Random Image and On click Image change
+var myPix = new Array(
+	"assets/1.jpg",
+	"assets/2.jpg",
+	"assets/3.jpg",
+	"assets/4.jpg",
+	"assets/5.jpg"
+);
 
 function choosePic() {
 	var randomNum = Math.floor(Math.random() * myPix.length);
 	document.getElementById("pic").src = myPix[randomNum];
 }
-document.addEventListener('DOMContentLoaded', function() {
-    var image = document.getElementById('image');
-    // onClick's logic below:
-    image.addEventListener('click', function() {
-        choosePic();
+document.addEventListener("DOMContentLoaded", function () {
+	var image = document.getElementById("image");
+	// onClick's logic below:
+	image.addEventListener("click", function () {
+		choosePic();
 		document.getElementById("image").contentWindow.location.reload(true);
-    });
+	});
+
+	var icon = document.getElementById("icon");
+	// onClick's logic below:
+	icon.addEventListener("click", function () {
+		dark();
+		window.localStorage.setItem("dark-theme", true);
+	});
+	var dragTheme = window.localStorage.getItem("dark-theme");
+	if (dragTheme == "false") {
+		document.body.classList.add("light");
+	} else if (dragTheme == "false") {
+		document.body.classList.remove("light");
+	}
 });
-choosePic()
+choosePic();
+
+//* Search box
+const f = document.getElementById("form");
+const q = document.getElementById("query");
+const se = "https://google.com/search?q=";
+
+function submitted(event) {
+	event.preventDefault();
+	if (q.value.startsWith(":")) {
+		const query = q.value.substring(q.value.indexOf(":") + 1);
+
+		const url = "https://" + query;
+		const win = window.open(url, "_blank");
+		q.value = "";
+		win.focus();
+	} else if (q.value.startsWith("youtube:") || q.value.startsWith("yt:")) {
+		const query = q.value.substring(q.value.indexOf(":") + 1);
+
+		const url = "https://www.youtube.com/results?search_query=" + query;
+		const win = window.open(url, "_blank");
+		q.value = "";
+		win.focus();
+	} else if (
+		q.value.startsWith("duckduckgo:") ||
+		q.value.startsWith("ddg:")
+	) {
+		const query = q.value.substring(q.value.indexOf(":") + 1);
+		const url = "https://duckduckgo.com/?q=" + query;
+		const win = window.open(url, "_blank");
+		q.value = "";
+		win.focus();
+	} else if (q.value.startsWith("reddit:")) {
+		const query = q.value.substring(q.value.indexOf(":") + 1);
+		const url = "https://www.reddit.com/search/?q=" + query;
+		const win = window.open(url, "_blank");
+		q.value = "";
+		win.focus();
+	} else if (q.value.startsWith("facebook:") || q.value.startsWith("fb:")) {
+		const query = q.value.substring(q.value.indexOf(":") + 1);
+		const url = "https://www.facebook.com/search/top/?q=" + query;
+		const win = window.open(url, "_blank");
+		q.value = "";
+		win.focus();
+	} else {
+		const url = se + q.value;
+		const win = window.open(url, "_blank");
+		q.value = "";
+		win.focus();
+	}
+}
+
+f.addEventListener("submit", submitted);
+
+document.body.addEventListener("keypress", function () {
+	document.getElementById("query").focus();
+});
+
+//* Theme management functions
+//Todo: Save theme data on local storage
+function dark() {
+	document.body.classList.toggle("light");
+	if (document.getElementById("icon").innerHTML == "filter_drama") {
+		document.getElementById("icon").innerHTML = `nights_stay`;
+		window.localStorage.setItem("dark-theme", true);
+	} else {
+		document.getElementById("icon").innerHTML = `filter_drama`;
+		window.localStorage.setItem("dark-theme", false);
+	}
+}
